c02501a6e8c048dc9980e39b8778110dfc005412
Segun Olatide <olatidesegun@gmail.com>
2023-06-11 21:17:50 +0100

    Version 3 - Array added

--- index.js
+++ index.js
@@ -6,3 +6,45 @@ function myFunction() {
 
 demo2.innerHTML = 5 + 6;
 
+var output = ["document.write", "console.log", "window.alert", "alert()"];
+console.log(output);
+outPut.innerHTML = "JS Output = " + output;
+
+
+console.log(`half of 100 is ${100 / 2}`);
+
+function myFunction0() {
+    demo3.innerHTML = "I was hidden until click";
+    demo4.innerHTML = "I was hidden until click too";
+}
+
+function myFunction1() {
+    demo3.style.display = 'none';
+    demo4.style.display = 'none';
+}
+/* var x = 5;
+var y = 6;
+var z = x + y;
+variables.innerHTML = "The value of z is: " + z;*/
+
+let x = "5" + 2 + 3;
+console.log(x);
+
+{
+    var jk = 6;
+    var kj = 5;
+    var z = jk + kj;
+
+    // let hy = 5;
+    // let yh = 6;
+    // let z = hy + yh;
+}
+
+alert(z);
+
+const motoCars = ["Segun", "Volvo", "Mercedes"];
+motoCars[0] = "Toyota";
+motoCars.push("Audi");
+constant.innerHTML = motoCars;
+
+
